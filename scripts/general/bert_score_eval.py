2339fb461e2d653058247b38d8493b5f7f759a83
Peter Yu <2057325+yukw777@users.noreply.github.com>
2023-07-19 10:57:01 -0400

    Log bert score for each datapoint

--- scripts/general/bert_score_eval.py
+++ scripts/general/bert_score_eval.py
@@ -7,9 +7,41 @@ import wandb
 from torchmetrics.text.bert import BERTScore
 
 
-def eval(preds: list[str], target: list[str], bert_score: BERTScore) -> None:
+def eval(rows: list[dict], bert_score: BERTScore) -> None:
+    preds = [row["generated"] for row in rows]
+    target = [row["ground_truth"] for row in rows]
     eval_results = bert_score(preds, target)
+    table = wandb.Table(
+        columns=[
+            "frame_path",
+            "video_uid",
+            "clip_index",
+            "generated",
+            "ground_truth",
+            "bert_score_f1",
+            "bert_score_precision",
+            "bert_score_recall",
+        ]
+    )
+    for row, f1, precision, recall in zip(
+        rows,
+        eval_results["f1"],
+        eval_results["precision"],
+        eval_results["recall"],
+    ):
+        table.add_data(
+            row["frame_path"],
+            row["video_uid"],
+            row["clip_index"],
+            row["generated"],
+            row["ground_truth"],
+            f1,
+            precision,
+            recall,
+        )
+
     log_dict = {
+        "bert_score_table": table,
         "bert_score_mean_f1": np.array(eval_results["f1"]).mean(),
         "bert_score_mean_precision": np.array(eval_results["precision"]).mean(),
         "bert_score_mean_recall": np.array(eval_results["recall"]).mean(),
@@ -28,11 +60,7 @@ if __name__ == "__main__":
 
     with open(args.gen_narration_file, newline="") as f:
         reader = csv.DictReader(f)
-        preds: list[str] = []
-        target: list[str] = []
-        for row in reader:
-            preds.append(row["generated"])
-            target.append(row["ground_truth"])
+        rows = list(reader)
 
     bert_score = BERTScore(
         device=args.device,
@@ -42,4 +70,4 @@ if __name__ == "__main__":
         verbose=True,
     )
 
-    eval(preds, target, bert_score)
+    eval(rows, bert_score)
