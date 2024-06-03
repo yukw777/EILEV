0e7a4a00b5b80462857c7c3f3bfdcb88ad10a74d
Peter Yu <2057325+yukw777@users.noreply.github.com>
2024-06-02 21:01:20 -0400

    Pass args transparently from submit_train_v2.py to train_v2.py

--- slurm-scripts/train/submit_train_v2.py
+++ slurm-scripts/train/submit_train_v2.py
@@ -1,50 +1,37 @@
 import argparse
 import base64
-import os
 import subprocess
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--account", required=True)
 parser.add_argument("--partition", required=True)
-parser.add_argument("--model", required=True)
-parser.add_argument("--run_name", required=True)
 parser.add_argument("--num_gpus", required=True, type=int)
 parser.add_argument("--mem_per_gpu", required=True)
 parser.add_argument("--time", required=True)
-parser.add_argument("--train_frames_dir", required=True)
-parser.add_argument("--train_annotation_file")
-parser.add_argument("--val_frames_dir", required=True)
-parser.add_argument("--val_annotation_file")
-parser.add_argument("--output_dir", required=True)
-parser.add_argument(
-    "--train_num_in_context_examples_per_sample", type=int, required=True
-)
-parser.add_argument("--val_num_in_context_examples_per_sample", type=int, required=True)
-parser.add_argument("--verb_noun_ratio", type=float, required=True)
-parser.add_argument("--random_in_context_examples", type=bool, default=False)
 parser.add_argument("--dataloader_num_workers", type=int, required=True)
 parser.add_argument("--train_batch_size", type=int, required=True)
 parser.add_argument("--per_device_train_batch_size", type=int, required=True)
-parser.add_argument("--per_device_eval_batch_size", type=int, required=True)
-parser.add_argument("--num_train_epochs", type=int, default=5)
-parser.add_argument("--warmup_steps", type=int, default=0)
 parser.add_argument("--email")
 parser.add_argument("--transformers_cache")
-parser.add_argument("--wandb_project", required=True)
-parser.add_argument("--resume_from_checkpoint", default=None)
+parser.add_argument("--wandb_project")
 parser.add_argument("--deepspeed_stage_2", action="store_true")
-parser.add_argument("--dry-run", action="store_true")
-args = parser.parse_args()
+parser.add_argument("--dry_run", action="store_true")
+args, train_args = parser.parse_known_args()
 
 email = ""
 if args.email is not None:
     email = f"#SBATCH --mail-user={args.email}\n#SBATCH --mail-type=BEGIN,END"
+
 transformers_cache = ""
 if args.transformers_cache is not None:
     transformers_cache = f"export TRANSFORMERS_CACHE={args.transformers_cache}"
-resume_from_checkpoint = ""
-if args.resume_from_checkpoint is not None:
-    resume_from_checkpoint = f"--resume_from_checkpoint {args.resume_from_checkpoint}"
+
+wandb_project = ""
+report_to_wandb = ""
+if args.wandb_project is not None:
+    wandb_project = f"export WANDB_PROJECT={args.wandb_project}"
+    report_to_wandb = "--report_to wandb"
+
 
 deepspeed = ""
 if args.deepspeed_stage_2:
@@ -80,21 +67,13 @@ gradient_accumulation_steps = (
     args.train_batch_size // args.per_device_train_batch_size // args.num_gpus
 )
 
-output_dir = os.path.join(args.output_dir, args.run_name)
-
-train_annotation_file = ""
-if args.train_annotation_file is not None:
-    train_annotation_file = f"--train_annotation_file {args.train_annotation_file}"
-
-val_annotation_file = ""
-if args.val_annotation_file is not None:
-    val_annotation_file = f"--val_annotation_file {args.val_annotation_file}"
+train_args_str = " ".join(train_args)
 
 script = rf"""#!/bin/bash
 
 #SBATCH --partition={args.partition}
 #SBATCH --time={args.time}
-#SBATCH --job-name=train-video-blip-v2
+#SBATCH --job-name=train-eilev
 {email}
 #SBATCH --account={args.account}
 #SBATCH --ntasks={args.num_gpus}
@@ -105,42 +84,17 @@ script = rf"""#!/bin/bash
 
 module load python/3.10.4 cuda
 {transformers_cache}
-export WANDB_PROJECT={args.wandb_project}
+{wandb_project}
 RDZV_ID=$RANDOM
 MASTER_NODE=$(srun --nodes=1 --ntasks=1 hostname)
 srun --cpus-per-task {args.dataloader_num_workers} poetry run torchrun --nnodes={args.num_gpus} --nproc_per_node=1 --rdzv-id=$RDZV_ID --rdzv-backend=c10d --rdzv-endpoint=$MASTER_NODE \
     ../../scripts/general/train_v2.py \
-    --model_name_or_path {args.model} \
-    --num_subsample_frames 8 \
-    --train_num_in_context_examples_per_sample {args.train_num_in_context_examples_per_sample} \
-    --val_num_in_context_examples_per_sample {args.val_num_in_context_examples_per_sample} \
-    --verb_noun_ratio {args.verb_noun_ratio} \
-    --random_in_context_examples {args.random_in_context_examples } \
-    --train_frames_dir {args.train_frames_dir} \
-    {train_annotation_file} \
-    --val_frames_dir {args.val_frames_dir} \
-    {val_annotation_file} \
-    --output_dir {output_dir} \
-    --num_train_epochs {args.num_train_epochs} \
-    --warmup_steps {args.warmup_steps} \
-    --learning_rate 1e-5 \
-    --per_device_train_batch_size {args.per_device_train_batch_size} \
-    --gradient_accumulation_steps {gradient_accumulation_steps} \
-    --ddp_find_unused_parameters False \
-    --per_device_eval_batch_size {args.per_device_eval_batch_size} \
-    --weight_decay 0.05 \
-    --dataloader_num_workers {args.dataloader_num_workers} \
-    --bf16 True \
+    {train_args_str} \
     {deepspeed} \
-    --evaluation_strategy steps \
-    --eval_steps 200 \
-    --save_strategy steps \
-    --save_steps 200 \
-    --save_total_limit 3 \
-    --logging_steps 10 \
-    --report_to wandb \
-    --run_name {args.run_name} \
-    {resume_from_checkpoint}
+    {report_to_wandb} \
+    --dataloader_num_workers {args.dataloader_num_workers} \
+    --per_device_train_batch_size {args.per_device_train_batch_size} \
+    --gradient_accumulation_steps {gradient_accumulation_steps}
 """  # noqa: E501
 print(script)
 if not args.dry_run:
